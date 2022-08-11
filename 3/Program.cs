9781850652bd9ded0b12ea0076c3f5e58b211a48
«Natali» <feichka@yandex.ru>
2022-08-11 17:39:01 +0300

    Add additional

---
+++ 3/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Задана целочисленная квадратная матрица размером N x N. 
+Требуется транспонировать ее относительно главной диагонали.
+
+Первая строка входного файла INPUT.TXT содержит натуральное число N – количество строк и столбцов матрицы. В каждой из последующих N строк записаны N целых чисел – элементы матрицы.
+ Все числа во входных данных не превышают 100 по абсолютной величине.
+*/
+
+int [,] array = new int[5,5];
+void PrintArray (int[,]array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+    { 
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write(($"{array [i, j]} ")); 
+        }
+        Console.WriteLine();
+    }
+} 
+
+void FillArray(int[,]array)
+{
+    {
+ for (int i = 0; i < array.GetLength(0); i++)
+ {
+ for (int j = 0; j < array.GetLength(1); j++)
+ {
+ array[i,j] = new Random().Next(1,10);
+ }
+ }
+}
+}
+
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+ {
+ for (int j = 0; j < array.GetLength(1); j++)
+
+{
+    if (i > j-1)
+    {
+    int x = array[i,j];
+    array[i,j] = array[j,i];
+    array[j,i] = x;
+    }
+ }
+}
+}
+Console.WriteLine();
+PrintArray(array);
